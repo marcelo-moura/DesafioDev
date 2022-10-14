97228b578e4331c97ebc9396b196b9093d5e1daa
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2022-10-14 19:00:46 -0300

    Refatoração na busca paginada de produtos

---
+++ DesafioDev/DesafioDev.Application/ViewModels/Entrada/FiltroProdutoViewModelEntrada.cs
@@ -0,0 +1,9 @@
+﻿namespace DesafioDev.Application.ViewModels.Entrada
+{
+    public class FiltroProdutoViewModelEntrada
+    {
+        public string? Nome { get; set; }
+        public string? Categoria { get; set; }
+        public decimal? Preco { get; set; }
+    }
+}
--- DesafioDev/DesafioDev.Infra.Common/Utils/Utils.cs
+++ DesafioDev/DesafioDev.Infra.Common/Utils/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using LinqKit;
+using System.ComponentModel;
 using System.Reflection;
 
 namespace DesafioDev.Infra.Common.Utils
@@ -22,6 +23,24 @@ namespace DesafioDev.Infra.Common.Utils
                 return attributes[0].Description;
             else
                 return source.ToString();
+        }
+
+        public static ExpressionStarter<TResult> MontarPredicateFiltro<TResult, T>(T objFiltro)
+        {
+            var predicate = PredicateBuilder.New<TResult>();
+
+            var filtroType = objFiltro.GetType();
+            foreach (var property in filtroType.GetProperties())
+            {
+                var field = property.Name;
+                var valor = property.GetValue(objFiltro, null);
+                if (valor != null && !string.IsNullOrEmpty(valor.ToString()))
+                {
+                    predicate = predicate.And(x => x.GetType().GetProperty(field).GetValue(x) != null &&
+                                                   x.GetType().GetProperty(field).GetValue(x).ToString().Contains(valor.ToString(), StringComparison.OrdinalIgnoreCase));
+                }
+            }
+            return predicate;
         }
     }
 }
