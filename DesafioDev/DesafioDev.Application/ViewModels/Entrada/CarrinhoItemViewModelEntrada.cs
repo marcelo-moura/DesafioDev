87f109dafac7a9873422d0955be0956ae4bbe59d
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2022-11-09 19:22:38 -0300

    Implementação do Middleware de Exception e alteração na CarrinhoController

--- DesafioDev/DesafioDev.Application/ViewModels/Entrada/CarrinhoItemViewModelEntrada.cs
+++ DesafioDev/DesafioDev.Application/ViewModels/Entrada/CarrinhoItemViewModelEntrada.cs
@@ -3,7 +3,7 @@
     public class CarrinhoItemViewModelEntrada
     {
         public Guid ProdutoId { get; set; }
-        public string ProdutoNome { get; set; }
+        public string NomeProduto { get; set; }
         public int Quantidade { get; set; }
         public decimal ValorUnitario { get; set; }
         public decimal ValorTotal { get; set; }
